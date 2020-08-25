402942638cc42fc196ca9af16216b4577904da09
jeXmin <rejesth.main@gmail.com>
2020-08-25 13:30:35 +0800

    FEAT: State-Page architecture

--- lib/core/architecture.dart/architecture.dart
+++ lib/core/architecture.dart/architecture.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 abstract class BibleJournalApp extends StatelessWidget {
@@ -26,5 +28,54 @@ abstract class BibleJournalMapper<T extends BibleJournalModel> {
       },
     );
     return list;
+  }
+}
+
+abstract class BibleJournalPageState<P extends BibleJournalPresenter>
+    extends State<BibleJournalPage> {
+  P presenter;
+
+  P createPresenter();
+
+  @override
+  void initState() {
+    presenter = createPresenter();
+    super.initState();
+    presenter.onViewInit();
+  }
+
+  @override
+  void dispose() {
+    presenter.dispose();
+    super.dispose();
+  }
+}
+
+abstract class BibleJournalPage extends StatefulWidget {
+  BibleJournalPage({Key key}) : super(key: key);
+
+  @override
+  BibleJournalPageState createState();
+}
+
+abstract class BibleJournalPresenter {
+  BibleJournalPageState bibleJournalPageState;
+
+  BuildContext get context => bibleJournalPageState.context;
+
+  BibleJournalPresenter(this.bibleJournalPageState);
+
+  List<StreamController> get streamControllers;
+
+  void onViewInit();
+
+  void dispose() {
+    streamControllers?.map((disposable) {
+      disposable.close();
+    });
+  }
+
+  void notify() {
+    bibleJournalPageState.presenter = this;
   }
 }
